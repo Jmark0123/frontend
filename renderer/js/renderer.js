2406f20f3d5b12177b9de5b6702b531c78ff3eec
jmark0123 <jmarkanthonylejista857@gmail.com>
2023-05-25 15:14:00 +0800

    adding some bootstrap in css and javascript

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -1,13 +1,32 @@
 const form = document.getElementById("form_sentence");
 if (form) {
-  form.onsubmit =  async function (e) {
+  form.onsubmit = async function (e) {
     e.preventDefault();
 
     const formData = new FormData(form);
+    let sentence = formData.get("sentence");
 
-    console.log(formData.get("sentence"));
-     
+    if (sentence.length <= 8) {
+      alertMessage("error", "Please input at least 8 characters!");
+      return;
+    }
+
     const response = await window.axios.openAI(formData.get("sentence"));
     document.getElementById("sentence_corrected").innerHTML = JSON.stringify(response.choices[0].text).replace(/\\n/g, '');
   };
+}
+
+function alertMessage(status, sentence){
+  window.Toastify.showToast({
+    text: sentence,
+    duration: 5000,
+    stopOnFocus: true,
+    style: {
+      textAlign: "center",
+      background: status == "error" ? "red":"green",
+      color: "white",
+      padding: "5px",
+      marginTop: "2px"
+    }
+  });
 }
